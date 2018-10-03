492b0a796b5a5b6fe687921515e0dc561e1dc709
davydov.a <davydov.a@cheb.eraga.net>
2018-10-03 15:00:26 +0300

    Download file and set vim configs

--- vim_config.sh
+++ vim_config.sh
@@ -2,10 +2,26 @@
 
 mkdir -p ~/.vim/colors
 if [ -x "$(command -v wget)" ]; then
-    echo 'wget'
+    wget -q https://raw.githubusercontent.com/tormozzg/configs/master/256-grayvim.vim -O ~/.vim/colors/256-grayvim.vim
+    echo 'Wget: Colors downloaded'
 elif [ -x "$(command -o curl)" ]; then
-    echo 'curl'
+    curl -s https://raw.githubusercontent.com/tormozzg/configs/master/256-grayvim.vim --output ~/.vim/colors/256-grayvim.vim
+    echo 'Curl: Colors downloaded'
 else
     echo 'Error: Please install wget or curl;'
     exit 1
 fi
+
+echo "set tabstop=2" >> ~/.vimrc
+echo "set shiftwidth=2" >> ~/.vimrc
+echo "set smarttab" >> ~/.vimrc
+echo "set wrap" >> ~/.vimrc
+echo "set ai" >> ~/.vimrc
+echo "set cin" >> ~/.vimrc
+echo "set showmatch" >> ~/.vimrc
+echo "set hlsearch" >> ~/.vimrc
+echo "set incsearch" >> ~/.vimrc
+echo "set ignorecase" >> ~/.vimrc
+echo "set number" >> ~/.vimrc
+echo "syntax on" >> ~/.vimrc
+echo "colorscheme 256-grayvim" >> ~/.vimrc
