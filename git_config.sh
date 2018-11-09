517db20c6e1d03e06aad65cad97558e4864c0852
tormozzg <dark.mozzg@gmail.com>
2018-11-09 18:29:57 +0400

    Update git_config.sh

--- git_config.sh
+++ git_config.sh
@@ -5,31 +5,31 @@ if ! [ -x "$(command -v git)" ]; then
     exit 1
 fi
 
-git config alias.ls  "log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate"
-git config alias.ll  "log --pretty=format:%C(yellow)%h\\ %C(green)%ad\\ %Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --numstat --date=rfc2822"
-git config alias.lsg "log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --graph"
-git config alias.lsg "= log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --graph --all"
-git config alias.cp  "cherry-pick"
-git config alias.st  "status"
-git config alias.stl "status -s"
-git config alias.cl  "clone"
-git config alias.ci  "commit"
-git config alias.com "commit --all"
-git config alias.co  "checkout"
-git config alias.br  "branch"
-git config alias.br  "branch"
-git config alias.di  "diff"
-git config alias.dif "= diff --word-diff"
-git config alias.dc  "diff --cached"
-git config alias.sl  "stash list"
-git config alias.sa  "stash apply"
-git config alias.ss  "stash save"
-git config alias.lds "log --pretty=format:%C(yellow)%h\\ %C(green)%ad%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --date=rfc2822"
-git config color.ui true
-git config core.autocrlf input
-git config core.editor vim
-git config core.fileMode false
-git config core.quotepath false
-git config push.default current
+git config --global alias.ls  "log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate"
+git config --global alias.ll  "log --pretty=format:%C(yellow)%h\\ %C(green)%ad\\ %Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --numstat --date=rfc2822"
+git config --global alias.lsg "log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --graph"
+git config --global alias.lsg "= log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --graph --all"
+git config --global alias.cp  "cherry-pick"
+git config --global alias.st  "status"
+git config --global alias.stl "status -s"
+git config --global alias.cl  "clone"
+git config --global alias.ci  "commit"
+git config --global alias.com "commit --all"
+git config --global alias.co  "checkout"
+git config --global alias.br  "branch"
+git config --global alias.br  "branch"
+git config --global alias.di  "diff"
+git config --global alias.dif "= diff --word-diff"
+git config --global alias.dc  "diff --cached"
+git config --global alias.sl  "stash list"
+git config --global alias.sa  "stash apply"
+git config --global alias.ss  "stash save"
+git config --global alias.lds "log --pretty=format:%C(yellow)%h\\ %C(green)%ad%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --date=rfc2822"
+git config --global color.ui true
+git config --global core.autocrlf input
+git config --global core.editor vim
+git config --global core.fileMode false
+git config --global core.quotepath false
+git config --global push.default current
 
 echo "Git config successfully applied"
