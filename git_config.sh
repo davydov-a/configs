d5ab36f63f6f86dc9bec8b190e7d5cb68df2d6b3
tormozzg <dark.mozzg@gmail.com>
2018-11-19 14:23:28 +0300

    Update git_config.sh

--- git_config.sh
+++ git_config.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v git)" ]; then
     exit 1
 fi
 
-git config --global alias.ls  "log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate"
-git config --global alias.ll  "log --pretty=format:%C(yellow)%h\\ %C(green)%ad\\ %Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --numstat --date=rfc2822"
-git config --global alias.lsg "log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --graph"
-git config --global alias.lsg "= log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --graph --all"
+git config --global alias.ls  "log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn] --decorate"
+git config --global alias.ll  "log --pretty=format:%C(yellow)%h\\ %C(green)%ad\\ %Cred%d\\ %Creset%s%Cgreen\\ [%cn] --decorate --numstat --date=rfc2822"
+git config --global alias.lsg "log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn] --decorate --graph"
+git config --global alias.lsga "log --pretty=format:%C(yellow)%h%Cred%d\\ %Creset%s%Cgreen\\ [%cn] --decorate --graph --all"
 git config --global alias.cp  "cherry-pick"
 git config --global alias.st  "status"
 git config --global alias.stl "status -s"
@@ -24,7 +24,7 @@ git config --global alias.dc  "diff --cached"
 git config --global alias.sl  "stash list"
 git config --global alias.sa  "stash apply"
 git config --global alias.ss  "stash save"
-git config --global alias.lds "log --pretty=format:%C(yellow)%h\\ %C(green)%ad%Cred%d\\ %Creset%s%Cgreen\\ [%cn]--decorate --date=rfc2822"
+git config --global alias.lds "log --pretty=format:%C(yellow)%h\\ %C(green)%ad%Cred%d\\ %Creset%s%Cgreen\\ [%cn] --decorate --date=rfc2822"
 git config --global color.ui true
 git config --global core.autocrlf input
 git config --global core.editor vim
